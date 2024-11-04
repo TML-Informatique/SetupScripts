fe178d0d406d4e45f5947bec606047762ecabd25
dbeaulieu-tml <167806863+dbeaulieu-tml@users.noreply.github.com>
2024-11-04 14:22:23 -0500

    Update AsSystemBeforeUsers.ps1

--- AsSystemBeforeUsers.ps1
+++ AsSystemBeforeUsers.ps1
@@ -1,1 +1,2 @@
-
+# not working
+if (Test-Connection -ComputerName 10.1.10.2 -Count 1) { net use S: \\10.1.10.2\files /no /user:guest tech; Copy-Item -Path "S:\files\Programs\MicrosoftOffice.zip" -Destination "C:\MicrosoftOffice.zip" }
